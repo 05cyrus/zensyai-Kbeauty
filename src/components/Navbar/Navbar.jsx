4cc21429b45a2c9f6361b56206a6726a156865de
Sumit Gusain <sumitgusain5311@gmail.com>
2025-01-27 23:44:10 +0530

    refined the navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <div className="logo" />
-        <span className="brand-text">GlowZy</span>
+        {/* <div className="logo" /> */}
+        <span className="brand-text">K-GlowZy</span>
       </div>
       <div className="navbar-menu">
         <a href="/">Whats k-beauty?</a>
